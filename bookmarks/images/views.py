66c60e983e22f347355e37cc943176df7de21d43
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-12-29 23:52:19 +0100

    Implemented the like/unlike button on image

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -3,6 +3,8 @@ from .models import Image
 from .forms import ImageCreateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
 
 
 @login_required
@@ -29,3 +31,23 @@ def image_detail(request, id, slug):
 
     return render(request, 'images/image/detail.html',
                   {'section': 'images', 'image': image})
+
+@login_required
+@require_POST
+def image_like(request):
+    """ Allow users to like and unlike images """
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+
+    return JsonResponse({'status': 'error'})
